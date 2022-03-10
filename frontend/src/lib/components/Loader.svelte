82f533004206c8a6e7caa6e26cff1ec279b9f7aa
TallestLegacy <tallestlegacy@gmail.com>
2022-03-10 18:11:39 +0300

    added asyn loading

---
+++ frontend/src/lib/components/Loader.svelte
@@ -0,0 +1,5 @@
+<script>
+	import { SyncLoader } from 'svelte-loading-spinners';
+</script>
+
+<SyncLoader color="#0a2749" size="60" />
--- frontend/src/routes/[id].svelte
+++ frontend/src/routes/[id].svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<!-- <script context="module">
 	import { getReviews, getProductData } from '$lib/queries';
 	export const load = async ({ params }) => {
 		const { id } = params;
@@ -22,25 +22,50 @@
 			};
 		}
 	};
+</script> -->
+<script context="module">
+	export const load = ({ params }) => {
+		const { id } = params;
+
+		return {
+			props: {
+				id
+			}
+		};
+	};
 </script>
 
 <script>
 	import ProductData from '$lib/components/ProductData.svelte';
 	import Review from '$lib/components/Review.svelte';
+	import Loader from '$lib/components/Loader.svelte';
+	import { page } from '$app/stores';
+	import { getReviews, getProductData } from '$lib/queries';
 
-	export let reviews;
-	export let product;
+	export let id;
 </script>
 
-<ProductData data={product} />
-<div class="reviews">
-	<h2>Reviews</h2>
+{#await getProductData(id)}
+	<Loader />
+{:then product}
+	<ProductData data={product} />
+	<pre>
+		{JSON.stringify(product)}
+	</pre>
+{/await}
+
+{#await getReviews(id)}
+	<Loader />
+{:then reviews}
 	<div class="reviews">
-		{#each reviews.docs as review}
-			<Review data={review} />
-		{/each}
+		<h2>Reviews</h2>
+		<div class="reviews">
+			{#each reviews.docs as review}
+				<Review data={review} />
+			{/each}
+		</div>
 	</div>
-</div>
+{/await}
 
 <style>
 	.reviews {
